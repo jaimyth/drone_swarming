33ccde191e65afa1bccfdc994e406382b1cf6fbe
jaimyth <jaimythielen@gmail.com>
2022-07-13 14:44:56 +0200

    Made environment class

---
+++ environment.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pygame as pg
+
+class Environment:
+
+    def __init__(self, width, height, bg_color):
+        self.width = width
+        self.height = height
+        self.agents = []
+        self.bg_color = bg_color
+        self.screen = self.initialise_environment()
+
+    def add_agent(self, agent):
+        self.agents.append(agent)
+
+    def update_agents(self):
+        for i in self.agents:
+            i.update()
+
+    def initialise_environment(self):
+        pg.init()
+        screen = pg.display.set_mode((self.width, self.height))
+        screen.fill(self.bg_color)
+        return screen
+
+    def update_environment(self):
+        pg.display.flip()
+
+    def clear_environment(self):
+        self.screen.fill(self.bg_color)
+
+    def draw_agents(self):
+        for i in self.agents:
+            i.draw(self.screen)
--- main.py
+++ main.py
@@ -1,38 +1,33 @@
 import pygame as pg
 import numpy as np
 from agent import Agent
+from environment import Environment
 import constants
 width, height = (800, 600)
 bg_color = (255, 200, 180)
 
-pg.init()
-screen = pg.display.set_mode((width, height))
-screen.fill(bg_color)
-
-
-ag1 = Agent()
-ag1.x, ag1.y = 100, 200
+env = Environment(width=width, height=height, bg_color=bg_color)
+env.update_environment()
+ag1 = Agent(100, 200)
 ag1.vx = 10
 
-ag1.draw(screen)
+env.add_agent(ag1)
 
-pg.display.flip()
 run =  True
-coords = [(ag1.x,ag1.y)]
+
 while run:
 
 
-    pg.time.delay(int(constants.dt*1000))
+
     t = pg.time.get_ticks()
-    screen.fill(bg_color)
     vy = 100*np.sin(t/1000)
     ag1.vy = vy
-    ag1.update()
-    coords.append((ag1.x, ag1.y))
-    pg.draw.lines(screen, (0,0,0), False, coords)
-    ag1.draw(screen)
-
+    env.clear_environment()
+    env.update_agents()
+    ag1.draw_history(env.screen)
+    env.draw_agents()
+    pg.time.delay(int(constants.dt*1000))
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
-    pg.display.flip()
+    env.update_environment()
